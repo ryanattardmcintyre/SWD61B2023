ed2a2d27e2e828189a95305d25720fbbb0bb9ec8
Ryan Attard <ryanattard@gmail.com>
2022-10-06 11:40:24 +0200

    lesson 1 - intro to classesAdd project files.

---
+++ Week1_IntroductionToClasses/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1_IntroductionToClasses
+{
+    class Person //is a definition (template) which defines
+                 //an entity by grouping a list members/behaviours which best describe that particular entity
+    {
+        //fields: characteristics which describe the entity (containers for data)
+        //note 1: a data type + an identifier
+        //note 2: fields are private
+        //note 3: (to adopt a good practice you must use) camel casing naming convention
+        DateTime dob;
+        string gender;
+        string firstName;
+        string lastName;
+        string address;
+
+        string id;
+        string email;
+        string mobile;
+        string password;
+
+
+        //properties: facilitate control of what goes inside the field and what is read from the field
+        //note 1: access modifier + data type + identifier + get + set
+        //note 2: properties are normally public
+        //note 3: (good practice) pascal case naming convention
+         public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+         public string LastName { get; set; } //shorthand notation
+
+        public string Id //longer (original)
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+ 
+
+    }
+}
---
+++ Week1_IntroductionToClasses/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1_IntroductionToClasses
+{
+    class Program
+    {
+        //1st thing that will run in my application
+        static void Main(string[] args) //this is compulsory in a Console type of Application
+        {
+            //Person is the class; in the class you cannot store the data
+            //p is an identifier which represents an object (instance); in the object you can store the data
+        
+            //the intellisense guides and facilitates coding
+            
+            Person p1 = new Person();
+          
+            p1.FirstName = "Joe"; // Joe is going to be stored/assigned into a property inside an object called p1
+         
+
+            Person p2 = new Person();
+
+
+
+            Person p3 = new Person();
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine(); //waits for the user to input something and presses enter
+        }
+    }
+}
