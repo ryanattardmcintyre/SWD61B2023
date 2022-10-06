0194e597d5c969cb372cd39095ffbd194048ff43
Ryan Attard <ryanattard@gmail.com>
2022-10-06 12:59:15 +0200

    Lesson 1 - complete (Classes, Properties, Fields, methods, enum)

--- Week1_IntroductionToClasses/Person.cs
+++ Week1_IntroductionToClasses/Person.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace Week1_IntroductionToClasses
 {
+    enum Reason {NoMatch, WeakPassword, Ok };
+
     class Person //is a definition (template) which defines
                  //an entity by grouping a list members/behaviours which best describe that particular entity
     {
@@ -13,38 +15,80 @@ namespace Week1_IntroductionToClasses
         //note 1: a data type + an identifier
         //note 2: fields are private
         //note 3: (to adopt a good practice you must use) camel casing naming convention
-        DateTime dob;
-        string gender;
+        
         string firstName;
-        string lastName;
-        string address;
-
-        string id;
-        string email;
-        string mobile;
         string password;
-
 
         //properties: facilitate control of what goes inside the field and what is read from the field
         //note 1: access modifier + data type + identifier + get + set
         //note 2: properties are normally public
         //note 3: (good practice) pascal case naming convention
-         public string FirstName
+        public string FirstName
         {
             get { return firstName; }
-            set { firstName = value; }
+            set
+            { 
+                firstName = value[0].ToString().ToUpper() + value.ToString().ToLower().Substring(1);
+            }
         }
-       
-         public string LastName { get; set; } //shorthand notation
-
-    
-        public string Id
+        public string Password
         {
-            get { return id; }
-            set { id = value; }
+            get
+            {
+                string outgoingpassword = "";
+                foreach (char c in password)
+                {
+                    outgoingpassword += "*";
+                }
+                return outgoingpassword;
+            }
+            
+        }
+        public string LastName { get; set; } //shorthand notation implied you must not declare the field (because it assumes that there's the field hidden)
+        public DateTime Dob { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string Id { get; set; }
+        public string Address { get; set; }
+        public string FullName
+        {
+            get { return firstName + ' ' + LastName;  }
         }
 
- 
+
+        //methods
+
+        //access modifiers:
+        //public - accessible from everywhere
+        //private - accessible only from within the same class (the most secure one)
+        //protected - accessible only from within the same class and from within inherited classes
+        //internal - accessible from within the same project
+
+        //<access modifier> <return type> <method name> ([parameter1, parameter2, parameter3 ...])
+        public Reason ResetPassword(string pwd, string confirmationPassword)
+        {
+            if (pwd == confirmationPassword)
+            {
+                //in C# we have special characters like these:
+                //   \n : new line
+                //   \t : tab
+                //   \r : skips a line
+
+
+                if (System.Text.RegularExpressions.Regex.IsMatch(pwd, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*]).{8,}$"))
+                {
+                    password = pwd;
+                    return Reason.Ok;
+                }
+                else return Reason.WeakPassword;
+
+
+            }
+            else return  Reason.NoMatch;
+        }
+
+        
 
     }
 }
--- Week1_IntroductionToClasses/Program.cs
+++ Week1_IntroductionToClasses/Program.cs
@@ -17,10 +17,45 @@ namespace Week1_IntroductionToClasses
             //the intellisense guides and facilitates coding
             
             Person p1 = new Person();
-          
-            p1.FirstName = "Joe"; // Joe is going to be stored/assigned into a property inside an object called p1
-         
-          //  p1. = "ryan";
+
+            Console.WriteLine("Input your name");
+            p1.FirstName = Console.ReadLine();
+
+
+            Console.WriteLine("Input your surname");
+            p1.LastName = Console.ReadLine();
+
+
+            Console.WriteLine("Input your password");
+            string pwd= Console.ReadLine();
+
+            Console.WriteLine("Input your confirmation password");
+            string confirmationpwd = Console.ReadLine();
+
+            switch( p1.ResetPassword(pwd, confirmationpwd))
+            {
+                case Reason.Ok: 
+                    Console.WriteLine("Password was changed");
+                    break;
+
+                case Reason.NoMatch:
+                    Console.WriteLine("Passwords do not match");
+
+                    break;
+
+                case Reason.WeakPassword:
+                    Console.WriteLine("password is weak");
+                    break;
+            }
+             
+
+
+            //  Console.WriteLine("Input your password");
+            //  p1.Password = Console.ReadLine();
+
+            Console.WriteLine(p1.FullName);
+            Console.WriteLine("Please press any key to terminate program");
+            Console.ReadKey();
 
             Person p2 = new Person();
 
